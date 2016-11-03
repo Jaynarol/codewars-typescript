ecfacebb37b452c38022bf92571174d00ca8ade1
Jaynarol <jaynarol@gmail.com>
2016-11-03 20:09:10 +0700

    first commit

---
+++ app/First/First.js
@@ -0,0 +1,11 @@
+"use strict";
+var Calculator = (function () {
+    function Calculator() {
+    }
+    Calculator.prototype.add = function (x, y) {
+        return 0;
+    };
+    return Calculator;
+}());
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Calculator;
